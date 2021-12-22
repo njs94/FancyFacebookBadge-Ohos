6d85dc2c8557301cad5aa16cfd56f7e50ec1589f
njs94 <naveenjs94@gmail.com>
2021-12-22 12:05:24 +0530

    update sample app

--- entry/src/main/java/com/shashank/sony/fancyfacebookbadgelibrary/demo/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/shashank/sony/fancyfacebookbadgelibrary/demo/slice/MainAbilitySlice.java
@@ -27,57 +27,76 @@ import com.shashank.sony.fancyfacebookbadgelibrary.demo.ResourceTable;
 /**
  * MainAbility Slice first class to be invoked.
  */
-public class MainAbilitySlice extends AbilitySlice {
+public class MainAbilitySlice extends AbilitySlice implements Component.ClickedListener {
 
-    private int mCount = 0;
-    FacebookNotificationBadge mBadge = null;
-    FacebookNotificationBadge mBadge1 = null;
-    FacebookNotificationBadge mBadge2 = null;
-    FacebookNotificationBadge mBadge3 = null;
-    FacebookNotificationBadge mBadge4 = null;
-    FacebookNotificationBadge mBadge5 = null;
+    private int hahaCount = 0;
+    private int sadCount = 0;
+    private int loveCount = 0;
+    private int wowCount = 0;
+    private int angryCount = 0;
+    private int likeCount = 0;
+    FacebookNotificationBadge hahaBadge = null;
+    FacebookNotificationBadge sadBadge = null;
+    FacebookNotificationBadge angryBadge = null;
+    FacebookNotificationBadge wowBadge = null;
+    FacebookNotificationBadge loveBadge = null;
+    FacebookNotificationBadge likeBadge = null;
 
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_main);
 
-        mBadge = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_badge);
-        mBadge1 = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_badge1);
-        mBadge2 = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_badge2);
-        mBadge3 = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_badge3);
-        mBadge4 = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_badge4);
-        mBadge5 = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_badge5);
-        mBadge.setEmoji(Emoji.HAHA);
-        mBadge1.setEmoji(Emoji.LIKE);
-        mBadge2.setEmoji(Emoji.LOVE);
-        mBadge3.setEmoji(Emoji.WOW);
-        mBadge4.setEmoji(Emoji.SAD);
-        mBadge5.setEmoji(Emoji.ANGRY);
-        Button increase = (Button) findComponentById(ResourceTable.Id_button1);
-        increase.setClickedListener(new Component.ClickedListener() {
-            @Override
-            public void onClick(Component component) {
-                ++mCount;
-                mBadge.setNumber(mCount);
-                mBadge1.setNumber(mCount);
-                mBadge2.setNumber(mCount);
-                mBadge3.setNumber(mCount);
-                mBadge4.setNumber(mCount);
-                mBadge5.setNumber(mCount);
-            }
-        });
-        Button clear = (Button) findComponentById(ResourceTable.Id_button2);
-        clear.setClickedListener(new Component.ClickedListener() {
-            @Override
-            public void onClick(Component component) {
-                mCount = 0;
-                mBadge.setNumber(mCount);
-                mBadge1.setNumber(mCount);
-                mBadge2.setNumber(mCount);
-                mBadge3.setNumber(mCount);
-                mBadge4.setNumber(mCount);
-                mBadge5.setNumber(mCount);  }
-        });
+        hahaBadge = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_hahabadge);
+        sadBadge = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_sadbadge);
+        angryBadge = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_angrybadge);
+        wowBadge = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_wowbadge);
+        loveBadge = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_lovebadge);
+        likeBadge = (FacebookNotificationBadge) findComponentById(ResourceTable.Id_likebadge);
+        hahaBadge.setEmoji(Emoji.HAHA);
+        likeBadge.setEmoji(Emoji.LIKE);
+        loveBadge.setEmoji(Emoji.LOVE);
+        wowBadge.setEmoji(Emoji.WOW);
+        sadBadge.setEmoji(Emoji.SAD);
+        angryBadge.setEmoji(Emoji.ANGRY);
+    }
+
+    @Override
+    public void onClick(Component component) {
+        switch (component.getId()) {
+            case ResourceTable.Id_hahabadge:
+                hahaCount++;
+                hahaBadge.setNumber(hahaCount);
+                break;
+            case ResourceTable.Id_sadbadge:
+                sadCount++;
+                sadBadge.setNumber(sadCount);
+                break;
+            case ResourceTable.Id_angrybadge:
+                angryCount++;
+                angryBadge.setNumber(angryCount);
+                break;
+            case ResourceTable.Id_wowbadge:
+                wowCount++;
+                wowBadge.setNumber(wowCount);
+                break;
+            case ResourceTable.Id_likebadge:
+                likeCount++;
+                likeBadge.setNumber(likeCount);
+                break;
+            case ResourceTable.Id_lovebadge:
+                loveCount++;
+                loveBadge.setNumber(loveCount);
+                break;
+            case ResourceTable.Id_reset:
+                hahaCount = likeCount = angryCount = loveCount = wowCount = sadCount = 0;
+                hahaBadge.setNumber(hahaCount);
+                loveBadge.setNumber(loveCount);
+                likeBadge.setNumber(likeCount);
+                wowBadge.setNumber(wowCount);
+                sadBadge.setNumber(sadCount);
+                hahaBadge.setNumber(hahaCount);
+                break;
+        }
     }
 }
